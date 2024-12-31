68f78040e7d96603a82264837a29987b2e486ad9
tommybob <i.am.tommybob@gmail.com>
2024-12-31 16:02:14 +0000

    Added local storage cleanup

--- PropertyChecker.js
+++ PropertyChecker.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Torn Properties Manager
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Inserts a properties table on Torn's properties page
 // @author       beans_ [174079]
 // @match        https://www.torn.com/properties.php*
